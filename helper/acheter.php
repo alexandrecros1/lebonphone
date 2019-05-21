ea9747a80814c0412028c7fa0687fdcd2c0e10c2
Paul Colombier <paul.colombier@epsi.fr>
2019-05-21 09:52:59 +0200

    suite du changement sur la partie achat des crédits

--- helper/acheter.php
+++ helper/acheter.php
@@ -37,31 +37,26 @@ if(isset($_POST['ajouter100'])){
 }
 ?>
 <div class="badge-center">
-    <h3 class="text-center card-header">Crédits actuels : <?=$credit['0']->credit;?> <img src="../img/coin.png"></h3><br><br>
-    <h3 class="text-center card-header">Acheter du crédit en toute sécurité :</h3>
-    <h6 class="text-center card-header">(1 <img src="../img/coin.png" style="width: 15px"> = 2,00€)</h6><br>
+    <h3 class="text-center card-header">Crédits actuels sur le compte : <?=$credit['0']->credit;?> <img src="../img/coin.png"></h3><br>
+    <h3 class="text-center card-header">Acheter du crédit en toute sécurité avec notre partenaire : <br>
+        <img src="../img/paypal.png" style="width: 180px"></h3><br>
     <form action="../helper/acheter.php" method="POST">
         <div style="text-align: center">
-            <button type="submit" name="ajouter1" class="btn btn-success">02,00€
-                <img src="../img/paypal.png" style="width: 80px"> = 1 <img src="../img/coin.png" style="width:20px"></button><br><br>
+            <button type="submit" name="ajouter1" class="btn btn-success">02,00€ = 1 <img src="../img/coin.png" style="width:20px"></button><br><br>
 
-            <button type="submit" name="ajouter2" class="btn btn-success">04,00€
-                <img src="../img/paypal.png" style="width: 80px"> = 2 <img src="../img/coin.png" style="width:20px"></button><br><br>
+            <button type="submit" name="ajouter2" class="btn btn-success">04,00€ = 2 <img src="../img/coin.png" style="width:20px"></button><br><br>
 
-            <button type="submit" name="ajouter5" class="btn btn-success">10,00€
-                <img src="../img/paypal.png" style="width: 80px"> = 5 <img src="../img/coin.png" style="width:20px"></button><br><br>
+            <button type="submit" name="ajouter5" class="btn btn-success">10,00€ = 5 <img src="../img/coin.png" style="width:20px"></button><br><br>
 
-            <button type="submit" name="ajouter10" class="btn btn-success">20,00€
-                <img src="../img/paypal.png" style="width: 80px"> = 10 <img src="../img/coin.png" style="width:20px"></button><br><br>
+            <button type="submit" name="ajouter10" class="btn btn-success">20,00€ = 10 <img src="../img/coin.png" style="width:20px"></button><br><br>
 
-            <button type="submit" name="ajouter25" class="btn btn-success">50,00€
-                <img src="../img/paypal.png" style="width: 80px"> = 25 <img src="../img/coin.png" style="width:20px"></button><br><br>
+            <button type="submit" name="ajouter25" class="btn btn-success">50,00€ = 25 <img src="../img/coin.png" style="width:20px"></button><br><br>
 
-            <button type="submit" name="ajouter50" class="btn btn-success">100,00€
-                <img src="../img/paypal.png" style="width: 80px"> = 50 <img src="../img/coin.png" style="width:20px"></button><br><br>
+            <button type="submit" name="ajouter50" class="btn btn-success">100,00€ = 50 <img src="../img/coin.png" style="width:20px"></button><br><br>
 
-            <button type="submit" name="ajouter100" class="btn btn-success">200,00€
-                <img src="../img/paypal.png" style="width: 80px"> = 100 <img src="../img/coin.png" style="width:20px"></button><br><br>
+            <button type="submit" name="ajouter100" class="btn btn-success">200,00€ = 100 <img src="../img/coin.png" style="width:20px"></button><br><br>
+
+            <h6 class="text-center card-header">(1 <img src="../img/coin.png" style="width: 15px"> = 2,00€)</h6>
         </div>
     </form>
 </div>
